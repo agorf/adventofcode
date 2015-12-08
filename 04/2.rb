56a37918295d6638a7a308f86b011c15c6eaec30
Angelos Orfanakos <me@agorf.gr>
2015-12-08 21:54:39 +0200

    Add fourth day

---
+++ 04/2.rb
@@ -0,0 +1,17 @@
+require 'digest/md5'
+
+secret_key = $stdin.read.chomp
+
+i = 1
+
+loop do
+  hash = Digest::MD5.hexdigest(secret_key + i.to_s)
+
+  if hash =~ /\A0{6}/
+    puts i
+    break
+  end
+
+  i += 1
+end
+
