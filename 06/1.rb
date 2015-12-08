35f71e2355eb354acd4b098fd9ba9b39feaa5c38
Angelos Orfanakos <me@agorf.gr>
2015-12-08 22:38:39 +0200

    Add sixth day

---
+++ 06/1.rb
@@ -0,0 +1,24 @@
+matrix = Array.new(1000) { Array.new(1000) { 0 } }
+
+$stdin.each_line do |line|
+  if data = line.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/)
+    cmd  = data[1]
+    from = data[2..3].map(&:to_i)
+    till = data[4..5].map(&:to_i)
+
+    from[0].upto(till[0]) do |i|
+      from[1].upto(till[1]) do |j|
+        matrix[i][j] = case cmd
+                       when 'turn on'
+                         1
+                       when 'turn off'
+                         0
+                       when 'toggle'
+                         1 - matrix[i][j]
+                       end
+      end
+    end
+  end
+end
+
+puts matrix.flatten.select {|n| n == 1 }.size
