c542671abc7763fd140bc2dbbdabbab715921ec1
Angelos Orfanakos <me@agorf.gr>
2015-12-10 23:38:46 +0200

    Replace chomp with chop
    No need to modify the line

--- 02/1.rb
+++ 02/1.rb
@@ -1,7 +1,7 @@
 sum = 0
 
 $stdin.each_line do |line|
-  l, w, h = line.chomp.split('x').map(&:to_i)
+  l, w, h = line.chop.split('x').map(&:to_i)
   products = [l*w, w*h, h*l]
   sum += products.map {|n| 2*n }.reduce(:+)
   sum += products.min
--- 02/2.rb
+++ 02/2.rb
@@ -1,7 +1,7 @@
 sum = 0
 
 $stdin.each_line do |line|
-  l, w, h = line.chomp.split('x').map(&:to_i)
+  l, w, h = line.chop.split('x').map(&:to_i)
   sum += [2*(l+w), 2*(w+h), 2*(h+l)].min
   sum += l*w*h
 end
