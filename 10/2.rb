4f54c3309366246969c4cf49426f2f049260aca8
Angelos Orfanakos <me@agorf.gr>
2015-12-10 15:08:51 +0200

    Add tenth day

---
+++ 10/2.rb
@@ -0,0 +1,11 @@
+def look_and_say(n)
+  n.to_s.scan(/((\d)\2*)/).map {|o, i| [o.size, i].join }.join.to_i
+end
+
+n = $stdin.read.chop.to_i
+
+50.times do
+  n = look_and_say(n)
+end
+
+puts n.to_s.size
