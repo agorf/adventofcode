73e8e54b2c66f47f6618265c9054e1857154c6f2
Angelos Orfanakos <me@agorf.gr>
2015-12-11 20:59:22 +0200

    Remove pointless assignments

--- 11/2.rb
+++ 11/2.rb
@@ -1,10 +1,9 @@
 password = $stdin.read.chop
 
 def valid_password?(pwd)
-  valid   = pwd !~ /[iol]/ # does not contain i, o, l
-  valid &&= pwd =~ Regexp.union(('a'..'z').each_cons(3).map(&:join)) # contains abc, bcd, ...
-  valid &&= pwd.scan(/(([a-z])\2)/).map(&:first).uniq.size >= 2 # contains at least two aa, bb, ..., zz
-  valid
+  pwd !~ /[iol]/ && # does not contain i, o, l
+    pwd =~ Regexp.union(('a'..'z').each_cons(3).map(&:join)) && # contains abc, bcd, ...
+    pwd.scan(/(([a-z])\2)/).map(&:first).uniq.size >= 2 # contains at least two aa, bb, ..., zz
 end
 
 first = true
