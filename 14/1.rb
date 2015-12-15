343c9743762e79fb46fc0e32692e3a3a7cd0de6b
Angelos Orfanakos <me@agorf.gr>
2015-12-15 11:12:22 +0200

    Make reindeer_distance more readable

--- 14/1.rb
+++ 14/1.rb
@@ -13,8 +13,10 @@ end
 def reindeer_distance(reindeer_specs, race_duration)
   round_duration = reindeer_specs.values_at(:fly_duration, :rest_duration).reduce(:+)
   round_distance = reindeer_specs.values_at(:fly_speed, :fly_duration).reduce(:*)
-  (race_duration / round_duration) * round_distance +
-    [(race_duration % round_duration) * reindeer_specs[:fly_speed], round_distance].min
+  whole_rounds   = race_duration / round_duration
+  seconds_left   = race_duration % round_duration
+
+  whole_rounds * round_distance + [seconds_left * reindeer_specs[:fly_speed], round_distance].min
 end
 
 puts reindeer_specs.values.map {|specs| reindeer_distance(specs, 2503) }.max
