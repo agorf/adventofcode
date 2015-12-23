9498442fb668cfdaaaff5c9a8e86a378356d84ce
Angelos Orfanakos <me@agorf.gr>
2015-12-24 00:31:56 +0200

    Use Enumerable#count instead of #reduce to count

--- 05/1.rb
+++ 05/1.rb
@@ -1,9 +1,3 @@
-count = $stdin.reduce(0) do |count, line|
-  if line =~ /([aeiou].*){3,}/ && line =~ /([a-z])\1/ && line !~ /(ab|cd|pq|xy)/
-    count + 1
-  else
-    count
-  end
-end
-
-puts count
+puts $stdin.count {|line|
+  line =~ /([aeiou].*){3,}/ && line =~ /([a-z])\1/ && line !~ /(ab|cd|pq|xy)/
+}
--- 05/2.rb
+++ 05/2.rb
@@ -1,9 +1,3 @@
-count = $stdin.reduce(0) do |count, line|
-  if line =~ /([a-z]{2}).*\1/ && line =~ /([a-z])[a-z]\1/
-    count + 1
-  else
-    count
-  end
-end
-
-puts count
+puts $stdin.count {|line|
+  line =~ /([a-z]{2}).*\1/ && line =~ /([a-z])[a-z]\1/
+}
