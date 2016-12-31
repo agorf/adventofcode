7b8181ee20c296c31ec9ca34a44ff843046ab550
Angelos Orfanakos <me@agorf.gr>
2016-12-31 20:51:39 +0200

    Add eighteenth day of 2016 in Go

---
+++ 2016/18/2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const rows = 400000
+
+func main() {
+	var line string
+
+	fmt.Scanln(&line)
+
+	width := len(line)
+	safeTiles := strings.Count(line, ".")
+
+	for i := 1; i < rows; i++ {
+		buf := make([]byte, width)
+
+		for j := 0; j < width; j++ {
+			leftSafe := j-1 < 0 || line[j-1] == '.'
+			centerSafe := line[j] == '.'
+			rightSafe := j+1 >= width || line[j+1] == '.'
+
+			trap := (!leftSafe && !centerSafe && rightSafe) || (leftSafe && !centerSafe && !rightSafe) || (!leftSafe && centerSafe && rightSafe) || (leftSafe && centerSafe && !rightSafe)
+
+			if trap {
+				buf[j] = '^'
+			} else {
+				buf[j] = '.'
+				safeTiles++
+			}
+		}
+
+		line = string(buf)
+	}
+
+	fmt.Println(safeTiles)
+}
