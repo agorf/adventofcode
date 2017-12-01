529419b5b7b5c3c86a917d7b10997a1376764b4e
Angelos Orfanakos <me@agorf.gr>
2017-12-01 16:00:39 +0200

    Add first day of 2017

---
+++ 2017/01/1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const stdin = process.stdin;
+
+stdin.resume();
+stdin.setEncoding('utf8');
+
+stdin.on('data', function (data) {
+  data = data.replace(/^\s+|\s+$/, '');
+
+  const size = data.length;
+
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    let next = i + 1;
+
+    if (next >= size) {
+      next = next % size;
+    }
+
+    if (data[i] === data[next]) {
+      sum += parseInt(data[i]);
+    }
+  }
+
+  console.log(sum);
+});
