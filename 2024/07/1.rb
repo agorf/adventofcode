eeb95f01f68011871a8c53d521638f685d537a62
Angelos Orfanakos <git@angelos.dev>
2024-12-09 22:34:32 +0200

    Add 1st part of 7th day of 2024

---
+++ 2024/07/1.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+OPS = %w[+ *].freeze
+
+def rpn(input)
+  stack = []
+
+  while (c = input.shift)
+    case c
+    when Integer
+      stack.push(c)
+    when '+'
+      stack.push(stack.pop + stack.pop)
+    when '*'
+      stack.push(stack.pop * stack.pop)
+    else
+      raise 'Unexpected character' # Should never happen
+    end
+  end
+
+  stack.first
+end
+
+def op_combos(numbers)
+  if numbers.size == 2
+    OPS.combination(1)
+  else
+    (numbers.size - 2).times.reduce(OPS.dup) do |result, _|
+      result.product(OPS)
+    end
+  end
+end
+
+def possible_values(numbers)
+  op_combos(numbers).map do |nested_ops|
+    rpn(numbers.reverse + nested_ops.flatten)
+  end
+end
+
+def count_value(test_value, numbers)
+  if possible_values(numbers).include?(test_value)
+    test_value
+  else
+    0
+  end
+end
+
+puts(
+  $stdin.each_line.reduce(0) do |sum, line|
+    test_value, *numbers = line.chomp.scan(/\d+/).map(&:to_i)
+    sum + count_value(test_value, numbers)
+  end
+)
